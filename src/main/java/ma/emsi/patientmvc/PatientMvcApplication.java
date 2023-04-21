98c1239703570e4e23bdbb6e850b220fd2fd4a06
rachidy <brachidy@gmail.com>
2023-04-21 16:25:46 +0000

    activity 3&4

---
+++ src/main/java/ma/emsi/patientmvc/PatientMvcApplication.java
@@ -0,0 +1,13 @@
+package ma.emsi.patientmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientMvcApplication.class, args);
+    }
+
+}
