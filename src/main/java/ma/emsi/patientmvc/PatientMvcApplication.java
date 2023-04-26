d976e968dad72dcd2de027c9c3488ae2313cc256
rachidy <brachidy@gmail.com>
2023-04-26 15:00:00 +0100

    latest

--- src/main/java/ma/emsi/patientmvc/PatientMvcApplication.java
+++ src/main/java/ma/emsi/patientmvc/PatientMvcApplication.java
@@ -17,20 +17,7 @@ public class PatientMvcApplication {
     public static void main(String[] args) {
         SpringApplication.run(PatientMvcApplication.class, args);
     }
-    CommandLineRunner commandLineRunner(PatientRepository patientRepository) {
-        return args -> {
-            patientRepository.save(new Patient(null, "Ikram", new Date(), false, 112));
-            patientRepository.save(new Patient(null, "Hanane", new Date(), true, 120));
-            patientRepository.save(new Patient(null, "Zaki", new Date(), false, 510));
-            patientRepository.save(new Patient(null, "Rayane", new Date(), true, 190));
-            patientRepository.findAll().forEach(
-                    p -> {
-                        System.out.println(p.getNom());
-                    }
-            );
 
-        };
-    }
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
